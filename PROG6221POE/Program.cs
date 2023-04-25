9b6c6d8274a8581c108685d22f5a5b301aa29fc2
lungelwa0 <103061058+lungelwa0@users.noreply.github.com>
2023-04-25 23:41:40 +0000

    Update Program.cs
    Improved array

--- PROG6221POE/Program.cs
+++ PROG6221POE/Program.cs
@@ -5,61 +5,52 @@ class Program
 {
      class Recipe
     {
-        int Select, option, count = 0, ingredientNum, numberOfSteps;
-        string[] recipe_Name = new string[9];
-        string[,] ingredient_Name = new string[9,9];
-        double[,] ingredient_Quantity = new double[9,9];
-        int[,] ingredient_UnitOfMeasurement = new int[9,9];
-        string[,] ingredient_Steps = new string[9,9];
-        int[,] recipeSteps = new int[9,9];
+        int Select, option, count = 0, ingredientNum, numberOfSteps, temp;
+        ArrayList ingredient_Steps = new ArrayList();
+        ArrayList ingredient_Name = new ArrayList();
         int[,] temp_Quantity = new int[9, 9];
-
-
+        ArrayList ingredient_Quantity = new ArrayList();
+        ArrayList ingredient_UnitOfMeasurement = new ArrayList();
         int menu()
         {
-            System.Console.WriteLine("(1) Enter details of a single recipe\n" +
+            Console.WriteLine("\n--MENU--");
+            Console.WriteLine("(1) Enter details of a single recipe\n" +
                 "(2) Display recipes\n" +
                 "(3) Increase serving of a recipe\n" +
                 "(4) Reset serving of a recipe\n" +
                 "(5) Delete a recipe\n" +
                 "(6) Exit Application");
-            System.Console.Write("Select Option: ");
+            Console.Write("\nSelect Option: ");
             Select = Convert.ToInt32(Console.ReadLine());
             return Select;
         }
         void addRecipe()
         {
-            Console.Write("\nEnter Recipe Name: ");
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-            string name = Console.ReadLine();
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-            recipe_Name[count] = name;
             Console.WriteLine("Enter Number of Ingredients Recipe Has: ");
             ingredientNum = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < ingredientNum; i++)
             {
-                System.Console.Write("Enter Ingredient Name:");
+                Console.Write("Enter Ingredient Name: ");
                 string ingredientName = Console.ReadLine();
-                ingredient_Name[count,i] = ingredientName;
-                Console.Write("Enter Ingerdient Quantity: ");
+                ingredient_Name.Add(ingredientName);
+                Console.Write("Enter Ingredient Quantity: ");
                 int ingredientQuantity = Convert.ToInt32(Console.ReadLine());
-                ingredient_Quantity[count, i] = ingredientQuantity;
-                temp_Quantity[count,i] = ingredientQuantity;
-                System.Console.Write("Enter Unit of Measurement for Ingredient: ");
-                int ingredientUnitOfMeasurement = Convert.ToInt32(Console.ReadLine());
-                ingredient_UnitOfMeasurement[count, i] = ingredientUnitOfMeasurement;
+                ingredient_Quantity.Add(ingredientQuantity);
+                temp = ingredientQuantity;
+                Console.Write("Enter Unit of Measurement for Ingredient: ");
+                string ingredientUnitOfMeasurement = Console.ReadLine();
+                ingredient_UnitOfMeasurement.Add(ingredientUnitOfMeasurement);
+                Console.WriteLine();
             }
             Console.WriteLine("\n******Ingredient Detail Successfully Saved!******\n" +
                         "Enter number of Steps for Recipe: ");
             numberOfSteps = Convert.ToInt32(Console.ReadLine());
-            recipeSteps[count, count] = numberOfSteps;
             Console.WriteLine("Enter Description of Each Step.");
             for (int j = 0; j < numberOfSteps; j++)
             {
                 Console.Write("Step " + (j+1) + ": ");
                 string ingredientSteps = Console.ReadLine();
-                ingredient_Steps[count, j] = ingredientSteps;
-
+                ingredient_Steps.Add(ingredientSteps);
             }
             Console.WriteLine("\n******Recipe Steps Successfully Saved!******\n");
             count++;
@@ -68,20 +59,17 @@ class Program
         {
             if (count != 0)
             {
-                for (int i = 0; i < count; i++)
+                Console.WriteLine("\n****** Ingredient List ******\n");
+                for (int i = 0; i < ingredientNum; i++)
                 {
-                    Console.WriteLine("\n******" + recipe_Name[i] + "******\n");
-                    Console.WriteLine("Ingredient List: ");
-                    for (int j = 0; j < ingredientNum; j++)
-                    {
-                        Console.WriteLine((j + 1) + "." + " (" + ingredient_Quantity[i, j] + ") " + ingredient_UnitOfMeasurement[i, j] + "ml/g " + ingredient_Name[i, j]);
-                    }
-                    Console.WriteLine("\nSTEPS:");
-                    for (int j = 0; j < recipeSteps[i, i]; j++)
-                    {
-                        Console.WriteLine("Step " + (j + 1) + ": " + ingredient_Steps[i, j]);
-                    }
+                    Console.WriteLine((i + 1) + "." + " (" + ingredient_Quantity[i] + ") " + ingredient_UnitOfMeasurement[i] + " of "+ ingredient_Name[i]);
                 }
+                Console.WriteLine("\nSTEPS:");
+                for (int i = 0; i < ingredient_Steps.Count; i++)
+                {
+                    Console.WriteLine("Step " + (i + 1) + ": " + ingredient_Steps[i]);
+                }
+                Console.WriteLine();
             }
             else
             {
@@ -90,62 +78,48 @@ class Program
         }
         void scaleUpRecipe()
         {
-            Console.WriteLine("\nSelect Option:");
-            Console.WriteLine("\n(1) By Half\n(2) Double Servings\n(3) Triple Servings");
+            Console.WriteLine("\nScale Up By:");
+            Console.WriteLine("\n(1) Half\n(2) Double \n(3) Triple ");
+            Console.Write("\nSelect Option: ");
             int choice = Convert.ToInt32(Console.ReadLine());
             if (choice == 1)
             {
-                for (int i = 0; i < ingredient_Quantity.GetLength(0); i++)
+                for (int i = 0; i < ingredient_Quantity.Count; i++)
                 {
-                    for( int j = 0; j < ingredient_Quantity.GetLength(1); j++)
-                    {
-                        ingredient_Quantity[i, j] *= 0.5;
-                    } 
+                    ingredient_Quantity[i] = temp * 0.5;
                 }
             }
             else if (choice == 2)
             {
-                for (int i = 0; i < ingredient_Quantity.GetLength(0); i++)
+                for (int i = 0; i < ingredient_Quantity.Count; i++)
                 {
-                    for (int j = 0; j < ingredient_Quantity.GetLength(1); j++)
-                    {
-                        ingredient_Quantity[i, j] *= 2;
-                    }
+                    ingredient_Quantity[i] = temp * 2;
                 }
             }
             else if (choice == 3) {
-                for (int i = 0; i < ingredient_Quantity.GetLength(0); i++)
+                for (int i = 0; i < ingredient_Quantity.Count; i++)
                 {
-                    for (int j = 0; j < ingredient_Quantity.GetLength(1); j++)
-                    {
-                        ingredient_Quantity[i, j] *= 3;
-                    }
+                    ingredient_Quantity[i] = temp * 3;
                 }
             }
+            Console.WriteLine("\n******Recipe Scale Successfully Updated!******\n");
         }
         void reset()
         {
-            for (int i = 0; i < ingredient_Quantity.GetLength(0); i++)
+            for (int i = 0; i < ingredient_Quantity.Count; i++)
             {
-                for (int j = 0; j < ingredient_Quantity.GetLength(1); j++)
-                {
-                    ingredient_Quantity[i, j] = temp_Quantity[i, j];
-                }
+                ingredient_Quantity[i] = temp;
             }
             Console.WriteLine("\n*****Reset was Successful!*****\n");
         }
         void deleteRecipe()
         {
-            for (int i = 0; i < recipe_Name.GetLength(0); i++)
+            for (int j = 0; j < 9; j++)
             {
-                for (int j = 0; j < 9; j++)
-                {
-                    ingredient_Name[i, j] = "";
-                    ingredient_Quantity[i, j] = 0;
-                    ingredient_Steps[i, j] = "";
-                    ingredient_UnitOfMeasurement[i,j] = 0;
-                }
-                recipe_Name[i] = "";
+                ingredient_Name.Clear();
+                ingredient_Quantity.Clear();
+                ingredient_Steps.Clear();
+                ingredient_UnitOfMeasurement.Clear();
             }
             count = 0;
             Console.WriteLine("\n******Recipe Succesfully Deleted!******\n");
@@ -158,10 +132,9 @@ class Program
         }
         int welcome()
         {
-            Console.WriteLine("RECIPE LOGBOOK APPLICATION\n"
+            Console.WriteLine("RECIPE CONSOLE APPLICATION\n"
                         + "********************************************************************\n"
                         + "Enter (1) to launch menu or any other key to exit");
-
             option = Convert.ToInt32(Console.ReadLine()); 
             return option;
         }
